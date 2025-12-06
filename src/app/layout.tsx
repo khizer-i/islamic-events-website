606a5ccf10f2d4a826c47c7bf81c87d0e2fa8fbb
khizer-i <khizeriqbal0@gmail.com>
2025-12-06 14:57:28 +0000

    Update layout.tsx for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,7 +19,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        {children}
+
+        <footer className="mt-10 border-t border-slate-200 dark:border-slate-800 py-6 text-center text-xs text-slate-500 dark:text-slate-400">
+          © {new Date().getFullYear()} Islamic Events Calendar UK
+        </footer>
+        <Analytics />
+      </body>
     </html>
   );
 }
