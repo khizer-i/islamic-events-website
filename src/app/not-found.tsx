5f1503346d0704fdef7d8c8ce3f05f6208acebcf
khizer-i <khizeriqbal0@gmail.com>
2025-12-06 16:29:55 +0000

    Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <html>
+            <body>
+                <h1>404 – Page Not Found</h1>
+                <p>Sorry, we couldn’t find that page.</p>
+            </body>
+        </html>
+    );
+}
